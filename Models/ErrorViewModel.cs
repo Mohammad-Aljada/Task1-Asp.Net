f242adad68223b0c02cb53c0dba3ee9862bd2c23
Mohammad-Aljada <mohammadaljad.1234@gmail.com>
2024-08-19 17:51:51 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task1_startup.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
